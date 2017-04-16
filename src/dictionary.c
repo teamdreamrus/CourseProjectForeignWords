7aa8e4d290af5a7058865321e09e728aa0a58e3e
Алексей <Atazhanov.biz@gmail.com>
2017-04-16 17:46:27 +0700

    Creat fuction print name dictionary

--- src/dictionary.c
+++ src/dictionary.c
@@ -1,3 +1,34 @@
-SearchDictionary(){
-	
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <err.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char* argv[])
+{
+DIR* dir;
+struct dirent* de;
+
+if(argc == 1)
+errx(1, "Expected directory name\nUsage: %s DIRECTORY", *argv);
+
+else if(argc > 2)
+errx(1, "Extra arguments were specified\nUsage: %s DIRECTORY", *argv);
+
+if((dir = opendir(argv[1])) == NULL)
+err(1, "opendir: `%s'", argv[1]);
+
+printf("Printing contents of directory `%s'.\n", argv[1]);
+
+while((de = readdir(dir)) != NULL)
+printf("%s\n", de->d_name);
+
+if(errno == EBADF)
+err(1, "readdir: `%s'", argv[1]);
+
+if(closedir(dir) == -1)
+err(1, "closedir: `%s'", argv[1]);
+
+exit(0);
 }
